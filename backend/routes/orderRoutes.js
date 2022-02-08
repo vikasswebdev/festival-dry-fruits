69d8056b0e26968530c1e3f2a0236d05a6c18062
vikas patel <vikaspatelkherad@gmail.com>
2022-02-08 22:10:27 +0530

    creating Order Routes and controller

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,4 +1,22 @@
 import express from "express";
+import {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToDelivered,
+  updateOrderToPaid,
+} from "../controllers/orderController";
 const router = express.Router();
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route("/").post(protect, addOrderItems);
+
+router.route("/myorders").get(protect, getMyOrders);
+
+router.route("/:id").get(protect, getOrderById);
+
+router.route("/:id/pay").put(protect, updateOrderToPaid);
+
+router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
 
 export default router;
